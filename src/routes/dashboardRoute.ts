2e733121377de8e13af4d3b39606df7e16d465c0
josuenm <direct.josue@gmail.com>
2023-01-07 18:35:59 -0300

    feat: admin access route has been deleted

--- src/routes/dashboardRoute.ts
+++ src/routes/dashboardRoute.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { salesController } from "../controllers/dashboard/salesController";
 import { dashboardSignInController } from "../controllers/dashboardAccount/signInController";
 import { dashboardSignUpController } from "../controllers/dashboardAccount/signUpController";
-
 const route = express.Router();
 
 route.post("/signIn", dashboardSignInController);

8ad7d8757eb644ba88f83e071e15529e572b2532
josuenm <direct.josue@gmail.com>
2023-01-07 18:35:20 -0300

    feat: admin access controller has been deleted

--- src/routes/dashboardRoute.ts
+++ src/routes/dashboardRoute.ts
@@ -1,12 +1,10 @@
 import express from "express";
 import { salesController } from "../controllers/dashboard/salesController";
-import { dashboardAdminSignUpController } from "../controllers/dashboardAccount/adminSignUpController";
 import { dashboardSignInController } from "../controllers/dashboardAccount/signInController";
 import { dashboardSignUpController } from "../controllers/dashboardAccount/signUpController";
 
 const route = express.Router();
 
-route.post("/admin/signUp", dashboardAdminSignUpController);
 route.post("/signIn", dashboardSignInController);
 route.post("/signUp", dashboardSignUpController);
 
